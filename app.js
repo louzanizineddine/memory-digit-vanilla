ccd477123e2bd9cfeab15bd86f570f9221120d5b
“louzanizineddine” <louzanizineddine@gmail.com>
2022-02-11 12:57:23 +0000

    handeled the main logic

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // grabbing dom Elements
-
 const levelElement = document.querySelector('.current-level');
 const timerElement = document.querySelector('.timer');
 const digitDisplay = document.querySelector('.digit-display');
 const userInput = document.querySelector('.user-input');
-const submitBtn = document.querySelector('.submit-btn')
+const submitBtn = document.querySelector('.submit-btn');
+const againBtn = document.querySelector('.again-button');
 
-const LEVEL = 0;
+let LEVEL = 2;
 let choosenNumberByComputer = [];
-    
+let interval;
 
 const generateNumber = function(level) {
     const max = Math.pow(10 , level);
@@ -24,44 +24,89 @@ const generateNumber = function(level) {
 
 
 const displayComputerChoice =  function(level  , delay) {
+    // we prevent the user from typing in the input while displaying 
+    userInput.disabled = true
     choosenNumberByComputer = generateNumber(level)
+    console.log(choosenNumberByComputer)
     choosenNumberByComputer.forEach((digit  , i)  => {
         setTimeout(( ) => {
-            // we delett the h4 elemetns if exists 
-            if (digitDisplay.children.length != 0) {
-                digitDisplay.children[0].remove();
-            }
-            const element = document.createElement('h4');
-            element.innerHTML = digit;
-            digitDisplay.appendChild(element);
-
-            console.log(digitDisplay.children);
+            digitDisplay.textContent = digit
+            // console.log(`the ${i  + 1}`);
         }  , delay * (i + 1))
     })
+
+    setTimeout(() => {
+        startTimer()
+    }, (choosenNumberByComputer.length + 1) * delay)
+
+}
+
+const startTimer = function() {
+    // we activate the autofocus
+    userInput.autofocus = true
+    // we delete the last digit from the secreen 
+    digitDisplay.textContent = ''
+    // we allow the user from typing in the input while displaying 
+    userInput.disabled = false;
+    // we activate the click event listener 
+    allowUserToSubmit()
+    seconds = 10;
+    console.log(`Timer is starting now`)
+    timerElement.innerHTML = ""
+    interval = setInterval(() => {
+        timerElement.innerHTML = seconds;
+        seconds = seconds - 1;
+        if (seconds == -1) {
+            stopTimer(interval)
+        }
+    }, 1000)
+    // clearInterval(interval)
 }
 
 
-const arrayToStr = function(array) {
-    str = ""
-    array.forEach(digit => {
-        str += digit
-    })
-    return str
-}
-const evaluateUserInput = function(str) {
-    return str === arrayToStr(choosenNumberByComputer)
-}
-// we evaluate the user input in two cases 
-// if he submits the answer or the timer runs out
-
-const getUserInput = function() {
-    console.log(userInput.value)
-    console.log(evaluateUserInput(userInput.value))
+const stopTimer = function (x) {
+    clearInterval(x);
 }
 
-submitBtn.addEventListener('click' , getUserInput)
+const getUserInput = () => {
+    return userInput.value
+};
+
+const evaluateUserInput = function() {
+    const userInput = getUserInput();
+    const computerChoice = choosenNumberByComputer
+                            .toString()
+                            .replaceAll(',','');
+    console.log(`the user input is ${userInput}`);
+    console.log(`the computer choice is ${computerChoice}`)
+
+    return userInput === computerChoice
+}
+const allowUserToSubmit = function () {
+    submitBtn.addEventListener('click' , func1)
+}
+
+const func1 = function ( ) {
+    if (evaluateUserInput()){
+        stopTimer(interval);
+        console.log('good memory');
+        LEVEL  = LEVEL + 1;
+        userInput.value = "";
+        displayComputerChoice(LEVEL , 1000);
+    }
+    else {
+        stopTimer(interval);
+        console.log('wrong guess');
+        userInput.value = "";
+        displayComputerChoice(LEVEL , 1000);
+    }
+}
 
 
-displayComputerChoice(3 , 1000)
+displayComputerChoice(LEVEL , 1000);
 
-getUserInput()
+
+
+
+
+
